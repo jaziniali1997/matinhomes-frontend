5c3dac8901ad4028231d95749c75781a0eb4120e
sanaz tajik <sanaztaj8@gmail.com>
2025-12-11 16:47:50 +0330

    Feat: Add property cards details

---
+++ components/PropertyCards.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+interface PropertyDetails {
+  MlsStatus?: string;
+  YearBuiltEffective?: number | string;
+  PropertySubType?: string[];
+  ListingContractDate?: string;
+}
+
+interface Props {
+  property: PropertyDetails;
+}
+
+export default function PropertyCards({ property }: Props) {
+  const cards = [
+    {
+      id: 1,
+      title: 'Listing Status',
+      value: property.MlsStatus || 'N/A',
+      img: '/Image/icons/check-one.svg',
+    },
+    {
+      id: 2,
+      title: 'Built in',
+      value: property.YearBuiltEffective || 'N/A',
+      img: '/Image/icons/calendar-three.svg',
+    },
+    {
+      id: 3,
+      title: 'Building Type',
+      value: property.PropertySubType || 'N/A',
+      img: '/Image/icons/city-one.svg',
+    },
+    {
+      id: 4,
+      title: 'Builder Name',
+      value: property.ListingContractDate || 'N/A',
+      img: '/Image/icons/compass.svg',
+    },
+  ];
+
+  return (
+    <div className='my-10 w-fit grid grid-cols-2 md:grid-cols-4 gap-2'>
+      {cards.map((card) => (
+        <div
+          key={card.id}
+          className='bg-white rounded-[8px] flex flex-col items-start p-2 shadow-sm w-[160px] lg:w-[192px] h-[113px]'
+        >
+          <img src={card.img} alt={card.title} className='w-[24px] mb-5' />
+          <h2 className='text-[12px] text-[#4D4D4D]'>{card.title}</h2>
+          <p className='text-[15px] mt-2 text-[#061B2E] font-semibold'>
+            {card.value}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
