e11eb03a6d7b74311c738014e78a562516c451bc
sanaz tajik <sanaztaj8@gmail.com>
2025-12-14 15:41:17 +0330

    Feat: Add font red hat display

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.matinhomes.ca',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
